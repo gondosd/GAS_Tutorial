49a9612a0eb2353b3fe7e7c926fff5388064817c
Gondi888 <gondos.daniel@googlemail.com>
2026-09-08 17:48:46 +0200

    Lession 7. Player Controller Input handling

---
+++ GAS_Tutorial/Source/GAS_Tutorial/Private/Player/GAS_PlayerController.cpp
@@ -0,0 +1,71 @@
+﻿// Copyright Daniel Göndös 2026
+
+
+#include "GAS_Tutorial/Public/Player/GAS_PlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/Character.h"
+
+class UEnhancedInputLocalPlayerSubsystem;
+
+void AGAS_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (!IsValid(InputSubsystem)) return;
+
+	for (TObjectPtr<UInputMappingContext> Context : InputMappingContexts)
+	{
+		InputSubsystem->AddMappingContext(Context, 0);
+	}
+	
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	if (!IsValid(EnhancedInputComponent)) return;
+	
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::Jump);
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ThisClass::StopJumping);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::Look);
+}
+
+void AGAS_PlayerController::Jump()
+{
+	if (!IsValid(GetCharacter())) return;
+	
+	GetCharacter()->Jump();
+}
+
+void AGAS_PlayerController::StopJumping()
+{
+	if (!IsValid(GetCharacter())) return;
+	
+	GetCharacter()->StopJumping();
+}
+
+void AGAS_PlayerController::Move(const FInputActionValue& Value)
+{
+	if (!IsValid(GetPawn())) return;
+	
+	const FVector& MovementVector = Value.Get<FVector>();
+	
+	//Find which was is forward
+	const FRotator YawRotation (0.f, GetControlRotation().Yaw, 0.f); 
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	
+	GetPawn()->AddMovementInput(ForwardDirection, MovementVector.Y);
+	GetPawn()->AddMovementInput(RightVector, MovementVector.X);
+
+}
+
+void AGAS_PlayerController::Look(const FInputActionValue& Value)
+{
+	if (!IsValid(GetPawn())) return;
+	
+	const FVector& LookAxisVector = Value.Get<FVector>();
+	
+	AddYawInput(LookAxisVector.X);
+	AddPitchInput(LookAxisVector.Y);
+}
---
+++ GAS_Tutorial/Source/GAS_Tutorial/Public/Player/GAS_PlayerController.h
@@ -0,0 +1,41 @@
+﻿// Copyright Daniel Göndös 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GAS_PlayerController.generated.h"
+
+
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+UCLASS(Abstract)
+class GAS_TUTORIAL_API AGAS_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void SetupInputComponent() override;
+
+private:
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Input")
+	TArray<TObjectPtr<UInputMappingContext>> InputMappingContexts;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Input")
+	TObjectPtr<UInputAction> JumpAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Input")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Input")
+	TObjectPtr<UInputAction> LookAction;
+	
+	void Jump();
+	void StopJumping();
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	
+};
