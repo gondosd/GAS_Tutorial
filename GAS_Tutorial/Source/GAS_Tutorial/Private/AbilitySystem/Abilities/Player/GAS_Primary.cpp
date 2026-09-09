d6b25aad6cdc7f1f1595d0f026eb7ef343579b03
Gondi888 <gondos.daniel@googlemail.com>
2026-09-10 00:13:17 +0200

    Lession 25. Overlap test -> Primary Ability overlap debug and checking moved to C++ side

---
+++ GAS_Tutorial/Source/GAS_Tutorial/Private/AbilitySystem/Abilities/Player/GAS_Primary.cpp
@@ -0,0 +1,49 @@
+﻿// Copyright Daniel Göndös 2026
+
+
+#include "AbilitySystem/Abilities/Player/GAS_Primary.h"
+
+#include "Engine/OverlapResult.h"
+
+void UGAS_Primary::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+                                   const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UGAS_Primary::HitBoxOverlapTest()
+{
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(GetAvatarActorFromActorInfo());
+
+	//Ensure that the overlap test ignores the Avatar Actor
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActors(ActorsToIgnore);
+
+	FCollisionResponseParams ResponseParams;
+	ResponseParams.CollisionResponse.SetAllChannels(ECR_Ignore);
+	ResponseParams.CollisionResponse.SetResponse(ECC_Pawn, ECR_Block);
+
+	TArray<FOverlapResult> OverlapResults;
+	FCollisionShape Sphere = FCollisionShape::MakeSphere(HitBoxRadius);
+
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector() * HitBoxForwardOffset;
+	const FVector HitboxLocation = GetAvatarActorFromActorInfo()->GetActorLocation() + Forward + FVector(0.f, 0.f, HitBoxElevationOffset);
+
+	GetWorld()->OverlapMultiByChannel(OverlapResults, HitboxLocation, FQuat::Identity, ECC_Visibility, Sphere, QueryParams, ResponseParams);
+
+	if (bDrawDebugs)
+	{
+		DrawDebugSphere(GetWorld(), HitboxLocation, HitBoxRadius, 16, FColor::Red, false, 3.f);
+
+		for (FOverlapResult& Result : OverlapResults)
+		{
+			if (Result.GetActor())
+			{
+				FVector DebugLocation = Result.GetActor()->GetActorLocation();
+				DebugLocation.Z += 100.f;
+				DrawDebugSphere(GetWorld(), DebugLocation, 30.f, 10, FColor::Green, false, 3.f);
+			}
+		}
+	}
+}
---
+++ GAS_Tutorial/Source/GAS_Tutorial/Public/AbilitySystem/Abilities/Player/GAS_Primary.h
@@ -0,0 +1,35 @@
+﻿// Copyright Daniel Göndös 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/GAS_GameplayAbility.h"
+#include "GAS_Primary.generated.h"
+
+UCLASS()
+class GAS_TUTORIAL_API UGAS_Primary : public UGAS_GameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
+public:
+	
+	UFUNCTION(BlueprintCallable, Category = "GAS|Abilities")
+	void HitBoxOverlapTest();
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Abilities")
+	float HitBoxRadius = 100.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Abilities")
+	float HitBoxForwardOffset = 200.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Abilities")
+	float HitBoxElevationOffset = 20.f;
+	
+	
+	
+};
