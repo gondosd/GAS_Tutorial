2e303ed1ad104fe4cc8400f3435a233221b873c7
Gondi888 <gondos.daniel@googlemail.com>
2026-09-09 13:38:13 +0200

    Lession 14.  gameplay tags defined

---
+++ GAS_Tutorial/Source/GAS_Tutorial/Private/GameplayTags/GAS_Tags.cpp
@@ -0,0 +1,9 @@
+﻿#include "GameplayTags/GAS_Tags.h"
+
+namespace GASTags
+{
+	namespace GASAbilities
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Primary, "GASTags.GASAbilities.Primary", "Tag for the Primary Ability")
+	}
+}
---
+++ GAS_Tutorial/Source/GAS_Tutorial/Public/GameplayTags/GAS_Tags.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+namespace GASTags
+{
+	namespace GASAbilities
+	{
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Primary);
+	}
+}
