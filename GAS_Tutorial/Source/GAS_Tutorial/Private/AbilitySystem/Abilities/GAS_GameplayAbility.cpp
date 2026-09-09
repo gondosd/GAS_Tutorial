f65f5d2bcb53b70bcadd94dc62837e29648d0ba2
Gondi888 <gondos.daniel@googlemail.com>
2026-09-09 15:02:31 +0200

    Lession 17. Base ability class with debug functionality added

---
+++ GAS_Tutorial/Source/GAS_Tutorial/Private/AbilitySystem/Abilities/GAS_GameplayAbility.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright Daniel Göndös 2026
+
+
+#include "AbilitySystem/Abilities/GAS_GameplayAbility.h"
+
+void UGAS_GameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+	if (bDrawDebugs)
+		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, FString::Printf(TEXT("%s Ability is Activated"), *GetClass()->GetName()));
+}
---
+++ GAS_Tutorial/Source/GAS_Tutorial/Public/AbilitySystem/Abilities/GAS_GameplayAbility.h
@@ -0,0 +1,22 @@
+﻿// Copyright Daniel Göndös 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GAS_GameplayAbility.generated.h"
+
+
+UCLASS()
+class GAS_TUTORIAL_API UGAS_GameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS|Debug")
+	bool bDrawDebugs = false;
+};
