34f65de8f99e023c78d48cf8226109d0d97d8d5e
Gondi888 <gondos.daniel@googlemail.com>
2026-09-12 16:51:38 +0200

    Lession 44. Async Attribute change node

---
+++ GAS_Tutorial/Source/GAS_Tutorial/Private/Tasks/GAS_AttributeChangeTask.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright Daniel Göndös 2026
+
+
+#include "Tasks/GAS_AttributeChangeTask.h"
+
+#include "AbilitySystemComponent.h"
+
+UGAS_AttributeChangeTask* UGAS_AttributeChangeTask::ListenForAttributeChange(UAbilitySystemComponent* AbilitySystemComponent, FGameplayAttribute Attribute)
+{
+	UGAS_AttributeChangeTask* WaitForAttributeChangeTask = NewObject<UGAS_AttributeChangeTask>();
+	WaitForAttributeChangeTask->ASC = AbilitySystemComponent;
+	WaitForAttributeChangeTask->AttributeToListenFor = Attribute;
+	
+	if (!AbilitySystemComponent) //Göndi comment. this check should be as a first line, and no remove from root, because its not added anywhere
+	{
+		WaitForAttributeChangeTask->RemoveFromRoot();
+		return nullptr;
+	}
+	
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Attribute).AddUObject(WaitForAttributeChangeTask, &ThisClass::AttributeChanged);
+	
+	return WaitForAttributeChangeTask;
+}
+
+void UGAS_AttributeChangeTask::EndTask()
+{
+	//Göndi Comment: this Endtask function is not called i dont understand why do we need it.
+	if (ASC.IsValid())
+		ASC->GetGameplayAttributeValueChangeDelegate(AttributeToListenFor).RemoveAll(this);
+	
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UGAS_AttributeChangeTask::AttributeChanged(const FOnAttributeChangeData& Data)
+{
+	OnAttributeChanged.Broadcast(Data.Attribute, Data.NewValue, Data.OldValue);
+}
---
+++ GAS_Tutorial/Source/GAS_Tutorial/Public/Tasks/GAS_AttributeChangeTask.h
@@ -0,0 +1,33 @@
+﻿// Copyright Daniel Göndös 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "GAS_AttributeChangeTask.generated.h"
+
+struct FOnAttributeChangeData;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnAttributeChanged, FGameplayAttribute, Attribute, float, NewValue, float, OldValue);
+
+UCLASS()
+class GAS_TUTORIAL_API UGAS_AttributeChangeTask : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+	
+	public:
+	UPROPERTY(BlueprintAssignable)
+	FOnAttributeChanged OnAttributeChanged;
+	
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = true))
+	static UGAS_AttributeChangeTask* ListenForAttributeChange(UAbilitySystemComponent* AbilitySystemComponent, FGameplayAttribute Attribute);
+	
+	UFUNCTION(BlueprintCallable)
+	void EndTask();
+	
+	TWeakObjectPtr<UAbilitySystemComponent> ASC;
+	FGameplayAttribute AttributeToListenFor;
+	
+	void AttributeChanged(const FOnAttributeChangeData& Data);
+};
