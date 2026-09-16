9dc51947ff8f3bf9a12eb68705a87ad86b1cfd9d
Gondi888 <gondos.daniel@googlemail.com>
2026-09-16 16:00:20 +0200

    Lession 59. Projectiles are harming us and having flashy effects

---
+++ GAS_Tutorial/Source/GAS_Tutorial/Private/GameObjects/GAS_Projectile.cpp
@@ -0,0 +1,37 @@
+﻿// Copyright Daniel Göndös 2026
+
+
+#include "GameObjects/GAS_Projectile.h"
+
+#include "AbilitySystemComponent.h"
+#include "Characters/GAS_PlayerCharacter.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+AGAS_Projectile::AGAS_Projectile()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+
+	bReplicates = true;
+}
+
+void AGAS_Projectile::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	AGAS_PlayerCharacter* PlayerCharacter = Cast<AGAS_PlayerCharacter>(OtherActor);
+	if (!PlayerCharacter || !PlayerCharacter->IsAlive()) return;
+
+	UAbilitySystemComponent* AbilitySystemComponent = PlayerCharacter->GetAbilitySystemComponent();
+	if (!AbilitySystemComponent || !HasAuthority()) return;
+	
+	const FGameplayEffectContextHandle ContextHandle = AbilitySystemComponent->MakeEffectContext();
+	const FGameplayEffectSpecHandle SpecHandle = AbilitySystemComponent->MakeOutgoingSpec(DamageEffect, 1.f, ContextHandle);
+	//TODO: use the damage variable for the amount of damage cause.
+	
+	AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+	
+	SpawnImpactEffects_BP();
+	Destroy();
+}
---
+++ GAS_Tutorial/Source/GAS_Tutorial/Public/GameObjects/GAS_Projectile.h
@@ -0,0 +1,36 @@
+﻿// Copyright Daniel Göndös 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GAS_Projectile.generated.h"
+
+class UGameplayEffect;
+class UProjectileMovementComponent;
+
+UCLASS()
+class GAS_TUTORIAL_API AGAS_Projectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AGAS_Projectile();
+
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+	
+	UFUNCTION(BlueprintImplementableEvent, Category = "GAS|Projectile")
+	void SpawnImpactEffects_BP();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS|Damage", meta = (ExposeOnSpawn, ClampMin = 0.0));
+	float Damage{10.f};
+
+private:
+	
+	UPROPERTY(VisibleAnywhere, Category = "GAS|Projectile")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Damage")
+	TSubclassOf<UGameplayEffect> DamageEffect;
+
+};
