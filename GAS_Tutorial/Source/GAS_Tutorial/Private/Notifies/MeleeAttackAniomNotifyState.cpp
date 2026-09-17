970360c1f0408165146bb0b49adb3479a15b9279
Gondi888 <gondos.daniel@googlemail.com>
2026-09-17 22:11:16 +0200

    Lession 63. Melee Attack Notify State migrated to C++ side

---
+++ GAS_Tutorial/Source/GAS_Tutorial/Private/Notifies/MeleeAttackAniomNotifyState.cpp
@@ -0,0 +1,121 @@
+﻿// Copyright Daniel Göndös 2026
+
+
+#include "Notifies/MeleeAttackAniomNotifyState.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "KismetTraceUtils.h"
+#include "Characters/GAS_BaseCharacter.h"
+#include "GameplayTags/GAS_Tags.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UMeleeAttackAniomNotifyState::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime,
+                                              const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+
+	if (!MeshComp || !MeshComp->GetOwner()) return;
+
+	SendEventsToActors(MeshComp, PerformSphereTrace(MeshComp));
+}
+
+TArray<FHitResult> UMeleeAttackAniomNotifyState::PerformSphereTrace(USkeletalMeshComponent* MeshComp) const
+{
+	TArray<struct FHitResult> OutHits;
+
+	const FTransform SocketTransform = MeshComp->GetSocketTransform(SocketName);
+	const FVector Start = SocketTransform.GetLocation();
+	const FVector ExtendedSocketDirection = UKismetMathLibrary::GetForwardVector(SocketTransform.GetRotation().Rotator() * SocketExtensionOffset);
+	const FVector End = Start - ExtendedSocketDirection;
+
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(MeshComp->GetOwner());
+
+	FCollisionResponseParams ResponseParams;
+	ResponseParams.CollisionResponse.SetAllChannels(ECR_Ignore);
+	ResponseParams.CollisionResponse.SetResponse(ECC_Pawn, ECR_Block);
+
+	UWorld* World = GEngine->GetWorldFromContextObject(MeshComp, EGetWorldErrorMode::LogAndReturnNull);
+	if (!World) return OutHits;
+
+	bool const bHit = World->SweepMultiByChannel(
+		OutHits,
+		Start,
+		End,
+		FQuat::Identity,
+		ECC_Visibility,
+		FCollisionShape::MakeSphere(SphereTraceRadius),
+		Params,
+		ResponseParams);
+
+	if (bDrawDebugs)
+	{
+		DrawDebugSphereTraceMulti(
+			World,
+			Start,
+			End,
+			SphereTraceRadius,
+			EDrawDebugTrace::ForDuration,
+			bHit,
+			OutHits,
+			FColor::Red,
+			FColor::Green,
+			5.f);
+	}
+
+	return OutHits;
+
+	//This was my implementation after this comment
+
+	// FVector Start;
+	// FRotator SocketRotation;
+	// MeshComp->GetSocketWorldLocationAndRotation(SocketName, Start, SocketRotation);
+	// const FVector End = Start - (SocketRotation.Vector() * SocketExtensionOffset);
+	//
+	// TArray<FHitResult> OutHits;
+	// TArray<AActor*> ActorsToIgnore;
+	// ActorsToIgnore.Add(MeshComp->GetOwner());
+	// UKismetSystemLibrary::SphereTraceMulti(MeshComp,
+	//                                        Start,
+	//                                        End,
+	//                                        SphereTraceRadius,
+	//                                        UEngineTypes::ConvertToTraceType(ECC_Visibility),
+	//                                        false,
+	//                                        ActorsToIgnore,
+	//                                        bDrawDebugs ? EDrawDebugTrace::ForDuration : EDrawDebugTrace::None,
+	//                                        OutHits,
+	//                                        true,
+	//                                        FLinearColor::Red,
+	//                                        FLinearColor::Green,
+	//                                        5.f);
+	//
+	// return OutHits;
+}
+
+void UMeleeAttackAniomNotifyState::SendEventsToActors(const USkeletalMeshComponent* MeshComp, const TArray<FHitResult>& Hits)
+{
+	for (const FHitResult& Hit : Hits)
+	{
+		FGameplayEffectContextHandle EffectContext;
+
+		if (const IAbilitySystemInterface* ASI = Cast<IAbilitySystemInterface>(MeshComp->GetOwner()))
+		{
+			const UAbilitySystemComponent* ASC = ASI->GetAbilitySystemComponent();
+			if (!ASC) continue;
+			EffectContext = ASC->MakeEffectContext();
+		}
+
+		EffectContext.AddHitResult(Hit);
+
+		AGAS_BaseCharacter* PlayerCharacter = Cast<AGAS_BaseCharacter>(Hit.GetActor());
+
+		if (!PlayerCharacter || !PlayerCharacter->IsAlive()) continue;
+
+		FGameplayEventData Payload;
+		Payload.Target = PlayerCharacter;
+		Payload.ContextHandle = EffectContext;
+		Payload.Instigator = MeshComp->GetOwner();
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(MeshComp->GetOwner(), GASTags::Events::Enemy::MeleeTraceHit, Payload);
+	}
+}
---
+++ GAS_Tutorial/Source/GAS_Tutorial/Public/Notifies/MeleeAttackAniomNotifyState.h
@@ -0,0 +1,35 @@
+﻿// Copyright Daniel Göndös 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "MeleeAttackAniomNotifyState.generated.h"
+
+UCLASS()
+class GAS_TUTORIAL_API UMeleeAttackAniomNotifyState : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime,
+		const FAnimNotifyEventReference& EventReference) override;
+
+private:
+	TArray<FHitResult> PerformSphereTrace(USkeletalMeshComponent* MeshComp) const;
+
+	static void SendEventsToActors(const USkeletalMeshComponent* MeshComp, const TArray<FHitResult>& Hits);
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS|Debugs", meta = (AllowPrivateAccess = true))
+	bool bDrawDebugs = true;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS|Trace", meta = (AllowPrivateAccess = true))
+	FName SocketName = FName("FX_Trail_01_R");
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS|Trace", meta = (AllowPrivateAccess = true))
+	float SocketExtensionOffset = 40.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS|Trace", meta = (AllowPrivateAccess = true))
+	float SphereTraceRadius = 60.f;
+};
